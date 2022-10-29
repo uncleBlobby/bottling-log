11a58baf4375c1cf13c238c93ebb54326f4de096
Dustin <dc.christianson@gmail.com>
2022-10-29 16:11:17 -0600

    added some ui functions to handle user inputs, monitor for missing inputs before processing data, and clearing inputs after submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { generateUUID } from './uuidGenerator';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -51,13 +52,16 @@ function App() {
     let LAA: number = 0;
     let liters: number = 0;
 
-    liters = ((parseFloat(size0Bottled) * .2) + (parseFloat(size1Bottled) * .375) + (parseFloat(size2Bottled) * .750) + (parseFloat(size3Bottled) * 1.14) + (parseFloat(size4Bottled) * 1.75))
+    if(abvInput){
+      liters = ((parseFloat(size0Bottled) * .2) + (parseFloat(size1Bottled) * .375) + (parseFloat(size2Bottled) * .750) + (parseFloat(size3Bottled) * 1.14) + (parseFloat(size4Bottled) * 1.75))
 
-    LAA = liters * (parseFloat(abvInput) / 100);
+      LAA = liters * (parseFloat(abvInput) / 100);
 
-    LAA = parseFloat(LAA.toFixed(2));
+      LAA = parseFloat(LAA.toFixed(2));
 
-    setLAABottled(LAA);
+      setLAABottled(LAA);
+    }
+    
     return LAA;
   }
 
@@ -72,42 +76,102 @@ function App() {
       size3qty: parseInt(size3Bottled),
       size4qty: parseInt(size4Bottled),
       totalLiters: litersBottled,
-      totalLAA: LAABottled
+      totalLAA: LAABottled,
+      uuid: generateUUID(),
+      createdAt: Date()
     }
 
-    console.log(entry);
+    
 
-    setLogEntries( logEntries => [...logEntries, entry]);
-    // TODO: figure out state variable for array of log entries
-    //setLogEntries(logEntries, ...entry);
+    if(entry.productName != "" && entry.date != "" && entry.abv != 0){
+      console.log(entry);
+      setLogEntries( logEntries => [...logEntries, entry]);
+    } else {
+      console.warn(`not a valid entry`);
+    }
+    
 
-    console.log(`state variable: ${JSON.stringify(logEntries)}`)
+    //console.log(`state variable: ${JSON.stringify(logEntries)}`)
+
+    clearInputRow();
+    
+    //sortLogEntries();
+  }
+
+  const sortLogEntries = () => {
+    let copy: BottlingEntry[] = logEntries;
+
+    //console.log(`sorted entries: ${JSON.stringify(copy)}`)
+  }
+
+  const clearInputRow = () => {
+    const d = document.getElementById('date') as HTMLInputElement;
+    const p = document.getElementById('product') as HTMLInputElement;
+    const a = document.getElementById('abvInput') as HTMLInputElement;
+    const b0 = document.getElementById('200mlBottled') as HTMLInputElement;
+    const b1 = document.getElementById('375mlBottled') as HTMLInputElement;
+    const b2 = document.getElementById('750mlBottled') as HTMLInputElement;
+    const b3 = document.getElementById('114LBottled') as HTMLInputElement;
+    const b4 = document.getElementById('175LBottled') as HTMLInputElement;
+
+    if (d && p && a && b0 && b1 && b2 && b3 && b4){
+      d.value = "";
+      p.value = "";
+      a.value = "";
+      b0.value = "";
+      b1.value = "";
+      b2.value = "";
+      b3.value = "";
+      b4.value = "";
+    }
+
+    setDateInput("");
+    setProductNameInput("");
+    setAbvInput("")
+    setSize0Bottled("")
+    setSize1Bottled("")     
+    setSize2Bottled("")     
+    setSize3Bottled("")     
+    setSize4Bottled("")     
+    setLitersBottled(0)    
+    setLAABottled(0)       
   }
 
   return (
     <div className="App">
       Bottling Log
-      {logEntries.map((entry, index) => {
-        return (
-          
-          <tr>
-            <td>{entry.date}</td>
-            <td>{entry.productName}</td>
-            <td>{entry.size0qty}</td>
-            <td>{entry.size1qty}</td>
-            <td>{entry.size2qty}</td>
-            <td>{entry.size3qty}</td>
-            <td>{entry.size4qty}</td>
-            <td>{entry.totalLiters}</td>
-            <td>{entry.totalLAA}</td>
-          
-          </tr>
-        )
-      })}
-      <table>
+      <table className='logOutputTable'>
         <thead>
           <tr>
           <th>Date</th><th>Product</th><th>abv</th><th>200ml</th><th>375ml</th><th>750ml</th><th>1.14L</th><th>1.75L</th><th>Total Liters</th><th>Total LAA</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logEntries.map((entry, index) => {
+            return (
+              
+              <tr key={entry.uuid}>
+                <td>{entry.date}</td>
+                <td>{entry.productName}</td>
+                <td>{entry.abv}</td>
+                <td>{entry.size0qty}</td>
+                <td>{entry.size1qty}</td>
+                <td>{entry.size2qty}</td>
+                <td>{entry.size3qty}</td>
+                <td>{entry.size4qty}</td>
+                <td>{entry.totalLiters}</td>
+                <td>{entry.totalLAA}</td>
+                <td>x</td>
+              </tr>
+            )
+        })}
+        </tbody>
+      </table>
+      
+      <table>
+        <thead>
+          <tr>
+          <th>Date</th><th>Product</th><th>abv</th><th>200ml</th><th>375ml</th><th>750ml</th><th>1.14L</th><th>1.75L</th>{/*<th>Total Liters</th><th>Total LAA</th>*/}
           </tr>
         </thead>
         <tbody>
@@ -121,9 +185,11 @@ function App() {
         <td><input id='114LBottled'   onChange={(evt) => setSize3Bottled(evt.target.value)}       className='qtyInput' placeholder=''></input></td>
         <td><input id='175LBottled'   onChange={(evt) => setSize4Bottled(evt.target.value)}       className='qtyInput' placeholder=''></input></td>
 
-
-        <td id="litersDisplay"        >{litersBottled} Liters</td>
-        <td id="litersAADisplay"      >{LAABottled} LAA</td>
+        {/*
+          <td id="litersDisplay"        >{litersBottled} Liters</td>
+          <td id="litersAADisplay"      >{LAABottled} LAA</td>
+        
+        */}
         <td><button onClick={() => createBottlingLogEntry()}>Add</button></td>
       </tr>
         </tbody>
---
+++ todos.md
@@ -0,0 +1,6 @@
+write function to sort the log entries by date value
+    automatically sort entries every time a new entry is added?
+
+build inventory module
+    subtract components from inventory with each bottling run
+
